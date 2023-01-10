59b1e61ddac30758dca8546f3edf37f45b0d274e
Heorhi Shkliaryk <H.Shkliaryk@sam-solutions.com>
2023-01-10 21:26:36 +0300

    refactor

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -184,7 +184,7 @@ const Quote = () => {
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2946.226900023373!2d-71.11670508430635!3d42.40161314046856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e376d8cfb914c3%3A0x9681ce0b21c872d0!2zMzEgUGVhcnNvbiBSZCwgU29tZXJ2aWxsZSwgTUEgMDIxNDQsINCh0KjQkA!5e0!3m2!1sru!2sby!4v1620771053814!5m2!1sru!2sby"
-        allowfullscreen=""
+        allowFullScreen={true}
         loading="lazy"
         title="map"
       ></iframe>
